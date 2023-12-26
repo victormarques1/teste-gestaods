a63f3a45aeb29345be1b27c81cdea2079b163408
Victor Marques <marques.vct@hotmail.com>
2023-12-26 14:58:07 -0300

    chore: add styled components and theme setup

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    blue: "rgba(19, 108, 220, 1)",
+    light_blue: "rgba(237, 243, 252, 1)",
+    purple: "rgba(79, 19, 104, 1)",
+    red: "rgba(197, 37, 37, 1)",
+    grey: "rgba(232, 232, 232, 1)",
+  },
+};
+
+export default theme;
