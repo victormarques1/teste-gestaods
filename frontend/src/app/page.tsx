8ff7509c8e8bca2e4695c9c6f3231ed51af7d6b5
Victor Marques <marques.vct@hotmail.com>
2023-12-31 14:32:35 -0300

    fix: refactor homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,31 @@
 "use client";
 
-import Image from "next/image";
+import { styled } from "styled-components";
 
-import "./globals.css";
-import { Container, TableContainer } from "./home-style";
+import Image from "next/image";
 import logo from "../../public/logo.png";
+
 import SearchBar from "@/components/SearchBar/SearchBar";
 import PatientTable from "@/components/PatientTable/PatientTable";
+import theme from "@/styles/theme";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  height: 100%;
+  background-color: ${theme.colors.background};
+`;
+
+const TableContainer = styled.div`
+  background-color: ${theme.colors.white};
+  width: 100%;
+  max-width: 1032px;
+  margin: 0 auto;
+  overflow-x: auto;
+  border-radius: 8px;
+`;
 
 const Home = () => {
   return (
